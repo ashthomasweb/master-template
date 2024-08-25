7aed7ba75cf9900c9982df8ee84e004470d6291e
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2024-08-24 18:43:25 -0700

    Adds styling to theme toggle

--- src/app/_components/theme-toggle.component.js
+++ src/app/_components/theme-toggle.component.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import {
     /* Firebase */
     /* Components */
@@ -39,22 +39,27 @@ export default function ThemeToggle() {
         }
     } = useContext(MainContext)
 
+    const [isChecked, setIsChecked] = useState(true)
+
     const handleThemeToggle = () => {
         ThemeService.switchTheme(theme)
+        theme === 'night' ? setIsChecked(false) : setIsChecked(true)
     }
 
     return (
         <div className='theme-toggle-container'>
-            <div className='icon-container'>
-                {theme === 'day'
-                    ? <SVGIcon src={iconPaths.sun}  />
-                    : <SVGIcon src={iconPaths.moon}  />
-                }
-            </div>
+
 
             <label className="switch">
-                <input type="checkbox" checked={theme === 'night'} onClick={handleThemeToggle} readOnly />
-                <span className="slider round"></span>
+                <input type="checkbox" checked={isChecked} onClick={handleThemeToggle} readOnly />
+                <span className="slider round">
+                    <div className={`icon-container ${isChecked ? 'isNight' : ''}`}>
+                        {theme === 'day'
+                            ? <SVGIcon src={iconPaths.sun} />
+                            : <SVGIcon src={iconPaths.moon} />
+                        }
+                    </div>
+                </span>
             </label>
         </div>
     )
